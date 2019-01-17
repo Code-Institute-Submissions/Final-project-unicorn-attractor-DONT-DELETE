72ecc9385f78d4bd073ee557c6b5f091111733c2
Russell Treagus <russell.treagus@hotmail.co.uk>
2019-01-17 13:47:02 +0000

    AWS storage services added.

---
+++ static/js/js.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#profile-edit").click(function(){
+        $(".profile-form").toggle();
+    });
+});
--- unicornApp/settings.py
+++ unicornApp/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
         },
@@ -131,15 +132,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 AWS_S3_OBJECT_PARAMETERS = {
-    'expires':'Thu, 31 Dec 2099 20:00:00 GMT',
-    'CacheControl':'max-age=94608000',
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
 }
 
-AWS_STORAGE_BUCKET_NAME = 'russ-unicornapp'
-AWS_S3_REGION_NAME = 'eu-london'
+AWS_STORAGE_BUCKET_NAME = 'unicornapp-bucket'
+AWS_S3_REGION_NAME = 'eu-west-2'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
+AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
@@ -148,18 +149,18 @@ STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-)
+    )
 
-PHOTOSFILES_LOCATION = 'photos'
-DEFAULT_FILE_STORAGE = 'custom_storages.PhotoStorage'
+MEDIAFILES_LOCATION = 'static/media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_URL = "/users/login/"
 LOGIN_REDIRECT_URL = 'home'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "photos")
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, PHOTOSFILES_LOCATION)
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 STRIPE_PUBLISHABLE = os.getenv("STRIPE_PUBLISHABLE")
 STRIPE_SECRET = os.getenv("STRIPE_SECRET")

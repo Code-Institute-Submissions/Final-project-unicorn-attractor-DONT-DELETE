4dd939fab488941702790c18b3667bd20c96cd6c
Russell Treagus <russell.treagus@hotmail.co.uk>
2019-01-17 14:15:25 +0000

    updated allowed users for heroku

--- unicornApp/settings.py
+++ unicornApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["unicornapp-milestone-project.herokuapp.com"]
 
 
 # Application definition

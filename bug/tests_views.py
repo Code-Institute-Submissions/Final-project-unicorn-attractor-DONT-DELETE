b5538261e53b169a359b1dad352717086bca8727
Russell Treagus <russell.treagus@hotmail.co.uk>
2019-03-07 12:55:01 +0000

    test views updated

--- bug/tests_views.py
+++ bug/tests_views.py
@@ -25,29 +25,36 @@ class TestBugViews(TestCase):
         self.assertTemplateUsed(response, 'create_bug.html')
 
     def test_preview_a_bug(self):
-        response = self.c.get('/bug/preview_bug/{0}'.format(self.bug.id), follow=True)
+        response = self.c.get('/bug/preview_bug/{0}'
+                              .format(self.bug.id), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'preview_bug.html')
 
     def test_bug_edit(self):
-        response = self.c.post('/bug/edit_bug/{0}/'.format(self.bug.id),
-                               {'title': 'test_bug_changed',
-                                'description': 'test_description',
-                                'status': 'Doing',
-                                'author_id': self.user.id
-                                }
-                               )
+        self.c.post('/bug/edit_bug/{0}/'
+                    .format(self.bug.id),
+                    {'title': 'test_bug_changed',
+                     'description': 'test_description',
+                     'status': 'Doing',
+                     'author_id': self.user.id
+                     }
+                    )
         new_updated_bug = get_object_or_404(Bug, id=1)
-        self.assertEqual(new_updated_bug.title, 'test_bug_changed')
-        self.assertEqual(new_updated_bug.description, 'test_description')
-        self.assertEqual(new_updated_bug.status, 'Doing')
+        self.assertEqual(new_updated_bug.title,
+                         'test_bug_changed')
+        self.assertEqual(new_updated_bug.description,
+                         'test_description')
+        self.assertEqual(new_updated_bug.status,
+                         'Doing')
 
     def test_upvote_bug(self):
-        response = self.c.get('/bug/upvote_bug/{0}'.format(self.bug.id), follow=True)
+        self.c.get('/bug/upvote_bug/{0}'
+                   .format(self.bug.id), follow=True)
         bug_upvoted = get_object_or_404(Bug, id=1)
         self.assertEqual(bug_upvoted.upvotes, 1)
 
     def test_add_a_bug_view(self):
-        response = self.c.get('/bug/add_bug/{0}'.format(self.bug.id), follow=True)
+        response = self.c.get('/bug/add_bug/{0}'
+                              .format(self.bug.id), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'add_bug.html')
